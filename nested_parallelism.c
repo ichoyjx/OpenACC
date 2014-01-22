b1ab19fb1c38e30f7a23d40e2e832176bcb79f76
Brian Yang <brianyang1106@gmail.com>
2014-01-22 13:31:44 -0600

    add example for NP

--- nested_parallelism.c
+++ nested_parallelism.c
@@ -13,5 +13,50 @@
  *     nested parallelism using parallel or kernels region
  *     containing another parallel or kernels region
  *
+ */
+
+/* Based on Micheal Wolfe's slides:
+ * http://www.openacc.org.php53-2.ord1-1.websitetestlink.com/sites/default/files/PGI-Wolfe-GTC-Notes.pdf
  *
+ */
+/////////////////// First ////////////////////
+//////////////////////////////////////////////
+#pragma acc routine
+extern void matvec(float* v,float* x,... );
+...
+#pragma acc parallel loop ...
+for( int i = 0; i < n; ++i )
+  matvec( v, x, i, n );
+//////////////////////////////////////////////
+#pragma acc routine
+matvec(...){
+#pragma acc parallel loop
+  for( int i = 0; i < n; ++i ){...}
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+/////////////////// Second ///////////////////
+//////////////////////////////////////////////
+#pragma acc routine
+  extern void matvec(float* v,float* x,... );
+  ...
+#pragma acc parallel num_gangs(1)
+    {
+      matvec( v0, x0, i, n );
+      matvec( v1, x1, i, n );
+      matvec( v2, x2, i, n );
+    }
+//////////////////////////////////////////////
+#pragma acc routine
+    matvec(...){
+#pragma acc parallel loop
+      for( int i = 0; i < n; ++i ){...}
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+/*
+ * The second case shows that accelerator creates a new
+ * thread to launch all the parallelism kernels.
+ *
+ * This is a good way, the point is ... ?
  */
