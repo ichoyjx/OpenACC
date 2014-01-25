a3062906b306b7542c6033db7569121fbe5a04c5
Brian Yang <brianyang1106@gmail.com>
2014-01-24 22:55:47 -0600

    second way to use enter/exit data

--- enter_exit.c
+++ enter_exit.c
@@ -15,3 +15,18 @@
 #pragma acc exit data delete(a[0:n*n], b[0:n]) copyout(c[0:n])
 
 // second
+void init(real_t *a, real_t *b, real_t *c, const int n)
+{
+  #pragma acc enter data copyin(a[0:n*n], b[0:n], c[0:n])
+}
+
+void fini(real_t *a, real_t *b, real_t *c, const int n)
+{
+  #pragma acc exit data delete(a[0:n*n], b[0:n]) copyout(c[0:n])
+}
+
+init(a, b, c, n);
+
+// ....
+
+fini(a, b, c, n);
