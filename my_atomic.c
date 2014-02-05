732d8d78ce7f9e8d4d129d6a72273f04885a338b
Brian Yang <brianyang1106@gmail.com>
2014-02-04 21:50:38 -0600

    OpenACC version

--- my_atomic.c
+++ my_atomic.c
@@ -9,3 +9,23 @@ int main() {
     printf("%d\n", 1>>32);
     return 0;
 }
+
+/*
+ * How does it work when running on GPU?
+ * here is the code snippet of the atomic test case
+ */
+int i;
+int x = 1;
+int tmp[n];
+#pragma acc parallel loop num_gangs(n)
+  for (i=0; i<n; i++) {
+#pragma acc atomic update
+    x = 1 << x;
+
+    tmp[i] = x;
+  }
+/*
+ * Is that supposed to be: 1, 2, 4, 16, 65536, ... what is the next?
+ * since OpenACC is implemented using CUDA, there should be a barrier
+ * after atomic.
+ */
