5c57d7d91c7e7d17520c280e05b34680592c8b96
Brian Yang <brianyang1106@gmail.com>
2014-02-04 21:43:00 -0600

    simple over width test

---
+++ my_atomic.c
@@ -0,0 +1,11 @@
+/*
+ * First, introduce a small shift operation code
+ * what is the result?
+ * http://codepad.org/pjtQUGKk
+ */
+#include <stdio.h>
+
+int main() {
+    printf("%d\n", 1>>32);
+    return 0;
+}
