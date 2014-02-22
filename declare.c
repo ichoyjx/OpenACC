f843ee8279d65f5262e52c611d610f91c1066f3b
Brian Yang <brianyang1106@gmail.com>
2014-02-22 00:41:36 -0600

    failed way to use

--- declare.c
+++ declare.c
@@ -2,3 +2,9 @@ float tmp[_SIZE];
 #pragma acc declare device_resident(tmp)
 float beta[2] = {42.0f, 84.0f};
 #pragma acc declare device_resident(beta)
+
+// wrong test
+//used as buffer between the 2 kernels, not accessible by host
+float tmp[10];
+#pragma acc declare device_resident(tmp)
+// why?
